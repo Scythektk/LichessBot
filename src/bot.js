c0cf41a4601290637352c356d7ab3aad3ccbbc7d
Scythektk <kartikechopra@gmail.com>
2021-04-10 12:08:55 +0530

    Matchup CrossTables

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 const { Client } = require('discord.js');
 const client = new Client();
 
+const lichess = require('./lichess');
+
 // !
 const PREFIX = '!';
 // !
@@ -13,38 +15,23 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-    // * Lichess
-    function runLichess(limit, clock_increment) {
-        axios
-            .post('https://lichess.org/api/challenge/open', {
-                clock: {
-                    limit: limit * 60,
-                    increment: clock_increment,
-                },
-            })
-            .then((res) => {
-                message.channel.send(
-                    `White: ${res.data.urlWhite}\nBlack: ${res.data.urlBlack}`
-                );
-                console.log(res.data.urlWhite);
-                console.log(res.data.urlBlack);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-    // *
-
     if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
     const [cmd_name, ...args] = message.content
         .trim()
         .substring(PREFIX.length)
         .split(/\s+/);
+
+    // Help
+    if (cmd_name === 'help') {
+        message.channel.send('```Try !game```');
+    }
+
+    // Lichess
     if (cmd_name === 'game') {
         const initial_time = Number(args[0]) || 5;
         const increment = Number(args[1]) || 0;
-        runLichess(initial_time, increment);
+        lichess.startGame(initial_time, increment);
     }
 });
 
